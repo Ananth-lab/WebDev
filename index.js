32e981f4a9dda321a1d687f389174ffef8fc5c07
Ananth <anantharaja@duck.com>
2022-10-12 17:44:21 +0530

    using JSON methods to store as Object

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ form.addEventListener("click", save)
 function save(e){
     if(e.target.classList.contains("submit")){
     let username = document.getElementById("username");
-    let password = document.getElementById("password")
-    localStorage.setItem(username.value, password.value)
-    console.log(username, password)
+    let password = document.getElementById("password");
+    let creds = {
+        name : username.value,
+        pass : password.value
+    }
+    let creds_serialised = JSON.stringify(creds)
+    localStorage.setItem("details", creds_serialised )
+    let creds_deserialised = JSON.parse(localStorage.getItem("details"));
+    console.log(creds_deserialised)
 }
 }
