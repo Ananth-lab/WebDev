5b8697d06454d816eb01958e1bc64907bf0e0b6c
Ananth <anantharaja@duck.com>
2022-11-05 00:44:18 +0530

    axios delete call

--- Appointment/index.js
+++ Appointment/index.js
@@ -3,7 +3,7 @@ const submit = document.querySelector("#Submit");
 submit.addEventListener("click", save)
 
 
-function showUserOnScreen(user){
+function showUserOnScreen(user) {
     let ul = document.querySelector("ul")
     let li = document.createElement("li");
     let edt = document.createElement("button")
@@ -17,13 +17,15 @@ function showUserOnScreen(user){
     li.appendChild(edt)
     li.appendChild(dlt)
     ul.appendChild(li)
-    dlt.addEventListener("click", (e) =>{
-        e.preventDefault();
-        parent = dlt.parentElement;
-        parent.remove()
+    dlt.addEventListener("click", (e) => {
+         e.preventDefault();
+         parent = dlt.parentElement;
+        //
+        let url = `https://crudcrud.com/api/24a574479da0477b941d7408b00ec791/appointmentData/${user._id}`
+        axios.delete(url).then( parent.remove()).catch(error => console.log(error))
     })
 
-    edt.addEventListener("click", (e) =>{
+    edt.addEventListener("click", (e) => {
         e.preventDefault();
         parent = dlt.parentElement;
         parent.remove()
@@ -42,13 +44,12 @@ document.addEventListener("DOMContentLoaded", () => {
     // }
 
     axios.get("https://crudcrud.com/api/24a574479da0477b941d7408b00ec791/appointmentData")
-    .then((response) => {
-        for(let i = 0; i < response.data.length; i++){
-            console.log(response.data[i])
-            showUserOnScreen(response.data[i])
-        }
-    })
-  })
+        .then((response) => {
+            for (let i = 0; i < response.data.length; i++) {
+                showUserOnScreen(response.data[i])
+            }
+        })
+})
 function save(e) {
     e.preventDefault();
     const name = document.querySelector("#name");
@@ -83,13 +84,18 @@ function save(e) {
 
 
 
-            dlt.addEventListener("click", (e) =>{
-                e.preventDefault();
+            dlt.addEventListener("click", (e) => {
+                 e.preventDefault();
                 parent = dlt.parentElement;
-                parent.remove()
+                // parent.remove()
+                let url = `https://crudcrud.com/api/24a574479da0477b941d7408b00ec791/appointmentData/${id}`
+                axios.delete(url).then(
+                    parent.remove()
+                )
+                .catch(error => console.log(error))
             })
 
-            edt.addEventListener("click", (e) =>{
+            edt.addEventListener("click", (e) => {
                 e.preventDefault();
                 parent = dlt.parentElement;
                 parent.remove()
@@ -100,6 +106,10 @@ function save(e) {
             let userDetailsString = JSON.stringify(userDetails)
             localStorage.setItem(`${userDetails.username}`, userDetailsString)
             axios.post("https://crudcrud.com/api/24a574479da0477b941d7408b00ec791/appointmentData", userDetails)
+                .then(response => {
+                    id = response.data._id;
+                })
+                .catch(error => console.log(error))
         }
     }
 }
