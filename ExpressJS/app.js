bf583810e72c1823ab5d1c03941fa9f5c52a902a
Ananth <anantharaja@duck.com>
2022-12-02 10:46:56 +0530

    started using middlewares and bodyparser

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 
+const bodyParser = require("body-parser");
 const app = express()
+app.use(bodyParser.urlencoded({extended:false}))
 
-app.use((req,res, next) => {
-    console.log("In the middleware")
-    next();
+
+app.use('/add-product',(req,res, next) => {
+    res.send(`<form action = "/product" method = "POST"><input type = "text" name = "title"> <input type = "number" name = "size"><button type = "submit">Add product</button></input></form>`)
 })
 
-app.use((req,res, next) => {
-    console.log("In the middleware2");
+app.post('/product',(req,res, next) => {
+    console.log(JSON.stringify(req.body));
+    res.redirect("/")
+})
+
+app.use('/',(req,res, next) => {
     res.send("<h1>Hello from Node JS</h1>")
 })
 
