60494c69ecc44262f72b288a9b5aef5512d7b794
Ananth <anantharaja@duck.com>
2022-12-29 17:08:03 +0530

    connected to db and added delete functionality

--- NodeJS_Shop_Project/controllers/admin.js
+++ NodeJS_Shop_Project/controllers/admin.js
@@ -14,8 +14,11 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save()
+  .then(() => {
+    res.redirect('/');
+  })
+  .catch(err => console.log(err))
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -55,7 +58,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([products]) =>{
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
@@ -66,6 +70,10 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.deleteById(prodId)
+  .then(() => {
+    res.redirect('/admin/products');
+  })
+  .catch(err => console.log(err))
+
 };
--- NodeJS_Shop_Project/util/database.js
+++ NodeJS_Shop_Project/util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host : "localhost",
     user:"root",
-    database: "node-complete",
+    database: "node-complte",
     password: "Naimisha@1999"
 });
 
