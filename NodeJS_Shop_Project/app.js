1cc3e081db6081434f9c80c46af9e073e8ac248d
Ananth <anantharajabk@gmail.com>
2023-02-15 20:33:40 +0530

    replaced schemas with mongoose

--- NodeJS_Shop_Project/app.js
+++ NodeJS_Shop_Project/app.js
@@ -2,9 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+
+require("dotenv").config()
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+
 const User = require('./models/user');
 
 const app = express();
@@ -18,20 +21,25 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('63eb4c4d020951703b5d7847')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id)
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('63eb4c4d020951703b5d7847')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id)
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+// mongoConnect(() => {
+//   app.listen(3000);
+// });
+
+mongoose.connect("mongodb+srv://ananth:Naimisha%401999@cluster0.gtxma2y.mongodb.net/shop?retryWrites=true&w=majority")
+.then(() => {
   app.listen(3000);
-});
+})
--- NodeJS_Shop_Project/controllers/shop.js
+++ NodeJS_Shop_Project/controllers/shop.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 const User = require("../models/user")
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
@@ -18,15 +18,6 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // Product.findAll({ where: { id: prodId } })
-  //   .then(products => {
-  //     res.render('shop/product-detail', {
-  //       product: products[0],
-  //       pageTitle: products[0].title,
-  //       path: '/products'
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
   Product.findById(prodId)
     .then(product => {
       res.render('shop/product-detail', {
@@ -39,7 +30,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('shop/index', {
         prods: products,
